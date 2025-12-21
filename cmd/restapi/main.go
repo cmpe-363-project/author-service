970e532a969be8125c3a992d7f2ddd77fae8da31
berk-karaal <karaalberk7@gmail.com>
2025-12-21 14:50:33 +0300

    update app version to v0.0.2.3

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -26,7 +26,7 @@ func main() {
 	repo := hardcodedrepository.NewHardcodedRepository()
 
 	app := &restapi.App{
-		Version:    "v0.0.2.2",
+		Version:    "v0.0.2.3",
 		Logger:     logger,
 		Repository: repo,
 		Port:       envVars.Port,
